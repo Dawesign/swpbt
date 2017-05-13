00831d15c46181841c4bfc51469bfc0b1b02931f
Markus Schmidt <markus262@web.de>
2017-05-13 19:41:26 +0200

    ugly url rel/abs hack

--- index.php
+++ index.php
@@ -25,14 +25,45 @@ $container['view'] = function ($container) {
 
 
 
-// catchall: '/[{path:.*}]' 
+// replace relative uris to absolute uris
+$mw_body = function($request, $response, $next) { 
+    $response = $next($request, $response);
+
+
+    if ( $_SERVER['HTTP_HOST'] =='trendiamo' ){
+		
+		$replacement = '="/';
+	
+	}else{
+		
+		$replacement = '="/swpbt/';
+	
+	}
+    
+    $body = $response->getBody();
+    
+    
+    // TODO: ugly HACK for frontentdesign
+	$body = str_replace('="../', $replacement  , $body );
+    
+    $body .= '<!-- hostname: '. $_SERVER['HTTP_HOST'] . ' -->';
+    
+    $newResp = new \Slim\Http\Response(); 
+    $newResp->write($body);
+
+    return $newResp; 
+};
+
+$app->add( $mw_body );
+
+
+// catchall: '/[{path:.*}]'
 
 $app->get('/', function($request, $response, $path = null) {
-
-    return $this->view->render($response, 'main.htm', [] );
+	
+	return $this->view->render($response, 'main.htm', [] );
     
 })->setName('start');
-
 
 
 $app->get('/product', function($request, $response, $path = null) {
@@ -40,7 +71,6 @@ $app->get('/product', function($request, $response, $path = null) {
     return $this->view->render($response, 'product.htm', [] );
     
 })->setName('productpage');
-
 
 
 // Run app
